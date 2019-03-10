13a7da4e69e90c7a42d7237a9a4a6b5d3350d676
Fernando Apolinário <fernandoapolinario@outlook.com>
2019-03-10 15:11:03 -0300

    RabbitMQ

---
+++ RabbitMQExamplePushisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQExemplePublisher
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Type something to send to the queue.");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                while (true)
+                {
+                    var message = Console.ReadLine();
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("Message sent. \n");
+                }
+            }
+        }
+    }
+}
